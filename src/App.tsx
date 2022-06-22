b142a01b72821c66ecaeff1d343f1ccd25c041f7
Jorge H Baptista <jorgehenrique@live.com>
2022-06-22 20:12:53 -0300

    Avançando no desenvolvimento Aula 02 - Ignite Lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
+import { Event } from "./pages/Event";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-interface Lesson {
-  id: string;
-  title: string;
-}
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY);
-  console.log(data);
-  return (
-    <ul>
-      {data?.lessons.map((lesson: Lesson) => {
-        return <li className="text-3xl" key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
+  return <Event></Event>;
 }
 
 export default App;
